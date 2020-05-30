f5ae154b2401e460939da26db68f5636b0edc3bb
apple <apple@appledeMacBook-Air-3.local>
2020-05-30 17:40:51 +0800

    5

--- src/views/TextUpload.vue
+++ src/views/TextUpload.vue
@@ -111,20 +111,153 @@
 </template>
 
 <script>
+    import * as d3 from "d3";
+
     export default {
         data() {
             return {
+                width: 800,
+                height: 800,
+                g:'',
+                svg:'',
                 textarea: '',
                 tid:'',
                 extractNode:'',
                 extractTeam:'',
-                tableData: []
+                tableData: [],
+                newGraph: {
+                    "nodes" : [{"name":"巴塞罗那","group":1,'country':'西班牙','year':1899},{"name":"皮克","group":2,'country':'西班牙','year':1987}
+                        ,{"name":"菲尔波","group":2,'country':'西班牙','year':1996},{"name":"格里兹曼","group":2,'country':'法国','year':1991},
+                        {"name":"弗兰基·德容","group":2,'country':'荷兰','year':1997},{"name":"阿图尔","group":2,'country':'巴西','year':1996},{"name":"乌姆蒂蒂","group":2,'country':'法国','year':1993},{"name":"罗贝托","group":2,'country':'西班牙','year':1992},{"name":"朗格莱","group":2,'country':'法国','year':1995},
+                        // {"name":"卡尔莱斯·佩雷兹","group":2,'country':'西班牙','year':1899},
+                        // {"name":"塞梅多","group":2},{"name":"布斯克茨","group":2},{"name":"安苏·法蒂","group":2}
+                        // {"id":"苏亚雷斯","group":2},{"id":"拉基蒂奇","group":2},{"id":"阿尔巴","group":2},{"id":"梅西","group":2},{"id":"特尔施特根","group":2}
+                    ],
+                    "links" : [{"source":"皮克","target":"巴塞罗那","value":"效力"}
+                        ,{"source":"菲尔波","target":"巴塞罗那","value":"效力"},{"source":"格里兹曼","target":"巴塞罗那","value":"效力"},{"source":"弗兰基·德容","target":"巴塞罗那","value":"效力"},{"source":"阿图尔","target":"巴塞罗那","value":"效力"},{"source":"乌姆蒂蒂","target":"巴塞罗那","value":"效力"},
+                        {"source":"罗贝托","target":"巴塞罗那","value":"效力"},{"source":"朗格莱","target":"巴塞罗那","value":"效力"}
+                        // {"source":"塞梅多","target":"巴塞罗那","value":"效力"},{"source":"布斯克茨","target":"巴塞罗那","value":"效力"},{"source":"安苏·法蒂","target":"巴塞罗那","value":"效力"},
+                        //     {"source":"苏亚雷斯","target":"巴塞罗那","value":"效力"},{"source":"拉基蒂奇","target":"巴塞罗那","value":"效力"},{"source":"阿尔巴","target":"巴塞罗那","value":"效力"},
+                        //     {"source":"梅西","target":"巴塞罗那","value":"效力"},{"source":"特尔施特根","target":"巴塞罗那","value":"效力"}
+                    ]
+
+                }
             }
         },
         mounted() {
             this.getAllTexts()
+            this.init()
+            this.initGraph(this.newGraph)
         },
         methods: {
+            //初始图画布
+            init:function(){
+                this.svg = d3.select(".temp_Graph")
+                    .append("svg")
+                    .attr("viewBox", [0,0,this.width  ,this.height ])
+                    .attr("height", 428)
+
+            },
+            //画图
+            initGraph(data) {
+                var _this = this
+                console.log(data)
+                this.svg.selectAll("*").remove();
+                const links = data.links.map(d => Object.create(d));
+                const nodes = data.nodes.map(d => Object.create(d));
+                const simulation = d3.forceSimulation(nodes)
+                    .force("link", d3.forceLink(links).id(d => d.name).distance(200))
+                    .force("charge", d3.forceManyBody().strength(-1500))
+                    .force("x", d3.forceX())
+                    .force("y", d3.forceY())
+                    .force("center", d3.forceCenter(this.width / 2 - 10, this.height / 2));
+
+                this.g = this.svg.append("g")
+                const link = this.g.append("g")
+                    .attr("stroke", "#999")
+                    .attr("stroke-opacity", 0.6)
+                    .selectAll("line")
+                    .data(links)
+                    .join("line")
+                    .attr("stroke-width", d => Math.sqrt(d.value));
+
+                const node = this.g.append("g")
+                    .attr("stroke", "#fff")
+                    .attr("stroke-width", 1.5)
+                    .selectAll("circle")
+                    .data(nodes)
+                    .join("circle")
+                    .attr("r", 30)
+                    .attr("fill", this.color())
+                    .on("click",_this.queryTest)
+                    .call(this.drag(simulation))
+
+                const nodeNameText = this.g.append("g")
+                    .selectAll("text")
+                    .data(nodes)
+                    .join("text")
+                    .attr("dx", (function (d) {
+                        return -d.name.length * 5
+                    }))
+                    .attr("dy", 1)
+                    .style('font-size', 12)
+                    // .style('font-weight', 400)
+                    .attr("fill","white")
+                    .attr("class", "node-name")
+                    .text(function (d) {
+                        return d.name
+                    })
+
+                simulation.on("tick", () => {
+                    link
+                        .attr("x1", d => d.source.x)
+                        .attr("y1", d => d.source.y)
+                        .attr("x2", d => d.target.x)
+                        .attr("y2", d => d.target.y);
+
+                    node
+                        .attr("cx", d => d.x)
+                        .attr("cy", d => d.y);
+
+                    nodeNameText
+                        .attr("x", d => d.x)
+                        .attr("y", d => d.y);
+                });
+
+            },
+            color() {
+                const scale = d3.scaleOrdinal(d3.schemeCategory10);
+                return d => scale(d.group);
+            },
+            queryTest(d){
+                console.log(d.name);
+                this.currentNode.country = d.country;
+                this.currentNode.year = d.year;
+                this.currentNode.name = d.name;
+            },
+            drag(simulation) {
+                function dragstarted(d) {
+                    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+                    d.fx = d.x;
+                    d.fy = d.y;
+                }
+
+                function dragged(d) {
+                    d.fx = d3.event.x;
+                    d.fy = d3.event.y;
+
+                }
+                function dragended(d) {
+                    if (!d3.event.active) simulation.alphaTarget(0);
+                    d.fx = null;
+                    d.fy = null;
+                }
+
+                return d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended);
+            },
             //获取所有未审核的节点
             getAllTexts:function(){
                 var _this = this
@@ -188,6 +321,10 @@
 </script>
 
 <style>
+    .temp_Graph{
+        background-color: #15161F;
+        /*height: 40%;*/
+    }
     .header_text{
         font-family: "PingFang SC";
         font-size: 30px;
@@ -209,13 +346,13 @@
 
     .container .left {
         width: 45%;
-        height: 100%;
+        height: 95%;
         position:absolute;
         background-color: #15161F;
     }
 
     .container .right {
-        height:100%;
+        height:95%;
         position:absolute;
         left: 45%;
         width: 55%;
